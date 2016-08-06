86ae372019a3accd16fd30defc7e77436117e986
Ballpin <ivan@it-smigo.se>
2016-08-06 14:54:17 +0200

    Added extract to css file and Clean directory

--- libs/parts.js
+++ libs/parts.js
@@ -1,8 +1,7 @@
 // Require Plugins
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-
-
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // Export BrowserSync
 exports.browserSync = function (paths) {
@@ -13,21 +12,40 @@ exports.browserSync = function (paths) {
                 // ./public directory is being served
                 host: 'localhost',
                 port: 3000,
-                server: {baseDir: [paths]}
+                server: {baseDir: [paths]},
+                open:false
             })
         ]
     }
 };
 
+// Export SASS & CSS & Style Settings
 exports.setupSASS = function () {
+    var extractCSS = new ExtractTextPlugin('assets/css/[name].css');
     return {
         module: {
             loaders: [
                 {
-                    test: /\.scss$/,
-                    loaders: ["style", "css", "sass"]
+                    test: /\.scss$/i,
+                    loader: extractCSS.extract(['css','sass'])
                 }
             ]
-        }
+        },
+        plugins: [
+            extractCSS
+        ]
+    }
+};
+
+// Clean Build Directory
+exports.setupClean = function (path) {
+    return {
+        plugins: [
+            new CleanWebpackPlugin(path, {
+                root: '/',
+                verbose: true,
+                dry: false
+            })
+        ]
     }
 };
